8281ca2002a132b559a72801dd3102fb69ab052e
Omer-iOSDeveloper <46841362+Omer-iOSDeveloper@users.noreply.github.com>
2019-01-23 16:15:47 +0500

    Delete SubCollectionViewDelegate.swift

--- Magneta Sqquare/D3CollectionView/SubCollectionViewDelegate.swift
+++
@@ -1,9 +0,0 @@
-//  Copyright © 2018 TheMysteryPuzzles.
-
-import UIKit
-
-@objc public protocol SubCollectionViewDelegate: class {
-    
-
-    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath)
-}
