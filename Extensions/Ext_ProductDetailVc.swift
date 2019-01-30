9d2830fa65b3c1eab9aaf407f0dab60a46a823b0
TheMysterPuzzles <omerkhann024@gmail.com>
2019-01-30 17:59:06 +0500

    New Features Addes.

---
+++ Extensions/Ext_ProductDetailVc.swift
@@ -0,0 +1,41 @@
+//
+//  Ext_ProductDetailVc.swift
+//  MagentoAPI
+//
+//  Created by Work on 1/30/19.
+//  Copyright © 2019 TheMysteryPuzzles. All rights reserved.
+//
+
+import UIKit
+
+extension ProductDetailViewController: ProductDetailDelegate {
+
+    func addToCartButtonTapped(withAttributes attributes: CustomAttributesViewModel) {
+        
+        self.model = ProductDetailModel(width: attributes.width, height: attributes.height, cordOperation: attributes.cordOperation, controlOperation: attributes.controlOperation, mountDetails: attributes.mountDetails, selectedColor: attributes.selectedColor)
+        
+        var controller = AddToCartViewController()
+        controller.selectedItem = self.selectedItem!
+        controller.selectedAttributes = CustomAttributes(width: model!.height, height: model!.width, cordOperation: model!.cordOperation, controlOperation: model!.controlOperation, mountDetails: model!.mountDetails, selectedColor: model!.selectedColor)
+       
+        self.navigationController?.pushViewController(controller, animated: true)
+    }
+    
+    
+    
+    func cancelButtonTapped() {
+        self.navigationController?.popViewController(animated: true)
+    }
+
+    
+    func productDetailLabelTapped() {
+        
+    }
+    
+    func colorSelected(selectedColor color: UIColor) {
+        
+    }
+    
+}
+
+
---
+++ Model/CustomAttributes.swift
@@ -0,0 +1,18 @@
+//
+//  CustomAttributes.swift
+//  MagentoAPI
+//
+//  Created by Work on 1/30/19.
+//  Copyright © 2019 TheMysteryPuzzles. All rights reserved.
+//
+
+import UIKit
+
+struct  CustomAttributes {
+    var width: Double
+    var height: Double
+    var cordOperation:CordOperation
+    var controlOperation: ControlOperation
+    var mountDetails: MountDetails
+    var selectedColor: UIColor
+}
